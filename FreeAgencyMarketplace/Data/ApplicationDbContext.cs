5dca7be75bbc6526354ee4d0397f1ff3798dded6
Sean Krispin <SKrispin@alas.com>
2021-11-18 07:57:28 -0600

    Added migration to further seed database with more players.

--- FreeAgencyMarketplace/Data/ApplicationDbContext.cs
+++ FreeAgencyMarketplace/Data/ApplicationDbContext.cs
@@ -19,10 +19,10 @@ namespace FreeAgencyMarketplace.Data
 		public DbSet<Team> Teams { get; set; }
 		public DbSet<Contract> Contracts { get; set; }
 
-		//So apparently this is how you seed your database with EF Core now. You need to override the OnModelCreating method and pass in an object of that entity type (as opposed to straight up Sql 
-		protected override void OnModelCreating(ModelBuilder builder)
+		//So apparently this is how you seed your database with EF Core now. You need to override the OnModelCreating method and pass in an object of that entity type (as opposed to straight up Sql() statements/methods)
+		protected override void OnModelCreating(ModelBuilder builder)//This method is called when the model for a derived context has been initialized, but before the model has been locked down and used to initialize the context. The default implementation of this method does nothing, but it can be overriden in a derived class such that the model can be further configured before it is locked down.
 		{
-			base.OnModelCreating(builder);
+			base.OnModelCreating(builder); //Not sure why that youtube tutorial had us remove the call to the base...
 
 			builder.Entity<Player>().HasData(
 					new Player()
@@ -31,6 +31,78 @@ namespace FreeAgencyMarketplace.Data
 						Name = "Justin Fields",
 						Position = "Quarterback",
 						Age = 22,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 2,
+						Name = "Roquan Smith",
+						Position = "Middle Linebacker",
+						Age = 25,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 3,
+						Name = "Byron Murphy",
+						Position = "Cornerback",
+						Age = 29,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 4,
+						Name = "Aaron Donald",
+						Position = "Defensive Tackle",
+						Age = 30,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 5,
+						Name = "Cooper Kupp",
+						Position = "Wide Receiver",
+						Age = 26,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 6,
+						Name = "Austin Ekeler",
+						Position = "Runningback",
+						Age = 27,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 7,
+						Name = "Fred Warner",
+						Position = "Middle Linebacker",
+						Age = 25,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 8,
+						Name = "J.J. Watt",
+						Position = "Outside Linebacker",
+						Age = 27,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 9,
+						Name = "Nick Bosa",
+						Position = "Defensive End",
+						Age = 28,
+						IsFreeAgent = false
+					},
+					new Player()
+					{
+						Id = 10,
+						Name = "Jamaal Adams",
+						Position = "Free Safety",
+						Age = 28,
 						IsFreeAgent = false
 					}
 				);
---
+++ FreeAgencyMarketplace/Data/Migrations/20211118135609_AddedPlayersToDatabase.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreeAgencyMarketplace.Data.Migrations
+{
+    public partial class AddedPlayersToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "Id", "Age", "IsFreeAgent", "Name", "Position" },
+                values: new object[,]
+                {
+                    { 2, 25, false, "Roquan Smith", "Middle Linebacker" },
+                    { 3, 29, false, "Byron Murphy", "Cornerback" },
+                    { 4, 30, false, "Aaron Donald", "Defensive Tackle" },
+                    { 5, 26, false, "Cooper Kupp", "Wide Receiver" },
+                    { 6, 27, false, "Austin Ekeler", "Runningback" },
+                    { 7, 25, false, "Fred Warner", "Middle Linebacker" },
+                    { 8, 27, false, "J.J. Watt", "Outside Linebacker" },
+                    { 9, 28, false, "Nick Bosa", "Defensive End" },
+                    { 10, 28, false, "Jamaal Adams", "Free Safety" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Players",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
