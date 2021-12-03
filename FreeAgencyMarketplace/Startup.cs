078e9311b2bad6635052477a6a91608c4e7d1e89
Sean Krispin <sean.krispin@gmail.com>
2021-12-02 22:39:18 -0600

    Added notes regarding the configuration of services/middleware in ConfigureServices() and Configure() methods.

--- FreeAgencyMarketplace/Startup.cs
+++ FreeAgencyMarketplace/Startup.cs
@@ -24,7 +24,9 @@ namespace FreeAgencyMarketplace
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
+		// This method gets called by the runtime. This method is used to add services to the container (and to configure those services). But, what are exactly services in the .NET Core world? Services should 
+		// be seen as a broad concept. A service is a component that is intended for common consumption in an application. There are framework services like Identity, MVC, Entity Framework Core services.
+		// But, there are also application services which are application specific, for example a component to send mail.
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddDbContext<ApplicationDbContext>(options =>
@@ -37,11 +39,15 @@ namespace FreeAgencyMarketplace
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		//Whenever an HTTP request comes in, something must handle that request and provide an HTTP response.
+		//The code that handles requests and provides responses make up the request pipeline.
+		//WHAT WE CAN DO IS CONFIGURE THAT REQUEST PIPELINE BY ADDING MIDDLEWARE WHICH IS SOFTWARE COMPONENTS (code) THAT ARE ASSEMBLED INTO AN APPLICATION PIPELINE TO HANDLE REQUESTS AND RESPONSES
+		//An example of middleware could be authentication (like Identity?)
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
-				app.UseDeveloperExceptionPage();
+				app.UseDeveloperExceptionPage(); //This statement configures the request pipeline by adding the DeveloperExceptionPage to it. So now when an exception is thrown this piece of middleware will handle it
 				app.UseDatabaseErrorPage();
 			}
 			else
@@ -53,8 +59,8 @@ namespace FreeAgencyMarketplace
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
-			app.UseRouting();
-
+			app.UseRouting();					//This is our HTTP request pipeline! See 2nd module lesson 5 & 6 in asp.net Core Web API pluralsight course
+												//Meaning this is how our ASP.NET Core MVC app is configured to handle HTTP requests
 			app.UseAuthentication();
 			app.UseAuthorization();
 
